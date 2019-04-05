505385c3f9c126e4935ec1f6ec939a255ada1f7d
Louis Quinnell <zioroboco@gmail.com>
2019-04-05 22:55:39 +1100

    Create basic elements package

---
+++ packages/elements/src/ExampleElement.tsx
@@ -0,0 +1,6 @@
+import { colours } from "@pyjamas/tokens"
+import * as React from "react"
+
+export const ExampleElement = () => (
+  <div style={{ color: colours.negative }}>@</div>
+)
---
+++ packages/elements/src/index.ts
@@ -0,0 +1,1 @@
+export { ExampleElement } from "./ExampleElement"
