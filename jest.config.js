962db4159b824dd4a2b014d5b93a41d5a9de7147
Louis Quinnell <zioroboco@gmail.com>
2019-04-06 20:05:37 +1100

    Add elm demo using example component

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testRunner: "jest-circus/runner",
   moduleNameMapper: {
+    "\\@demos/(.*)": "<rootDir>/demos/$1/src",
     "\\@pyjamas/(.*)": "<rootDir>/packages/$1/src"
   }
 }
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,2 +1,2 @@
-export { Accordion } from "./Accordion"
+export { Accordion, AccordionContent, AccordionTitle } from "./Accordion"
 export { ExampleComponent } from "./ExampleComponent"
