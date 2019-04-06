2c5f07fe287dc5a6e2c1adb61e1469c4b63481d0
Louis Quinnell <zioroboco@gmail.com>
2019-04-06 13:46:27 +1100

    Add a yarn reset script

---
+++ scripts/reset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eo pipefail
+
+test ! -z $INIT_CWD || (echo "❌ Please run me via \`yarn reset\`" && exit 1)
+
+root_dir=$(pwd)
+
+echo "Resetting ${root_dir}"
+
+clean() {
+  xargs -I {} -- bash -c "echo ... {} && rm -rf {}"
+}
+
+find $root_dir -maxdepth 1 -name "node_modules" | clean
+find $root_dir -maxdepth 3 -name "node_modules" | clean
+yarn
