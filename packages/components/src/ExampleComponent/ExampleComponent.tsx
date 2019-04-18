8e661f55aad04fb728707285bb4eb603897c59e7
Louis Quinnell <zioroboco@gmail.com>
2019-04-18 11:45:49 +1000

    Remove emotion-js dependency

--- packages/components/src/ExampleComponent/ExampleComponent.tsx
+++ packages/components/src/ExampleComponent/ExampleComponent.tsx
@@ -1,4 +1,3 @@
-import { css, jsx } from "@emotion/core"
 import { colours, fonts } from "@pyjamas/tokens"
 import * as React from "react"
 
@@ -6,12 +5,11 @@ export type Props = React.PropsWithChildren<{
   colour: keyof typeof colours
 }>
 
-const stylesheet = (props: Props) =>
-  css({
-    color: colours[props.colour],
-    fontSize: fonts.size.big
-  })
+const stylesheet = (props: Props) => ({
+  color: colours[props.colour],
+  fontSize: fonts.size.big
+})
 
 export const ExampleComponent: React.FunctionComponent<Props> = props => (
-  <span css={stylesheet(props)}>{props.children}</span>
+  <span style={stylesheet(props)}>{props.children}</span>
 )
