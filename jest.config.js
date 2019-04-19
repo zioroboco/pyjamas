4c343915eb78c0f301fecbc10986a6f1856fd0ef
Louis Quinnell <zioroboco@gmail.com>
2019-04-19 19:35:33 +1000

    Fix typo in jest module mapping for demo packages

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testRunner: "jest-circus/runner",
   moduleNameMapper: {
-    "\\@demos/(.*)": "<rootDir>/demos/$1/src",
+    "\\@demo/(.*)": "<rootDir>/demos/$1/src",
     "\\@pyjamas/(.*)": "<rootDir>/packages/$1/src"
   }
 }
